376619760ebba5a486fedde1cae358b8227e6e19
Youngjae Joo <bityoungjae@gmail.com>
2020-01-25 01:58:23 +0900

    fix queryParser.js : empty handling

--- queryParser.js
+++ queryParser.js
@@ -1,6 +1,7 @@
 const parseQuery = query => {
   const splitted = query.split('&');
   const mapped = splitted
+    .filter(q => !!q)
     .map(q => q.split('='))
     .map(([key, value]) => (value ? `${key} => ${value}` : key));
 
